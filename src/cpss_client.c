e668c1e2e41c068d51260fef4e73e2e5efc8530d
Troels Jessen <troels@space-inventor.com>
2026-06-29 11:42:17 +0200

    Align client helper to new IF

--- src/cpss_client.c
+++ src/cpss_client.c
@@ -3,10 +3,10 @@
 
 int32_t cpss_put(int node, uint32_t block_id, void * from, uint32_t length, rpc_protocol_t protocol, int timeout) {
 
-    rpc_cpss_put_block_element_rdp_request_t put_request = rpc_cpss_put_block_element_rdp_init(block_id, length);
-    rpc_cpss_put_block_element_rdp_response_t put_response;
+    rpc_cpss_put_block_element_request_t put_request = rpc_cpss_put_block_element_init(block_id, length);
+    rpc_cpss_put_block_element_response_t put_response;
 
-    rpc_cpss_put_block_element_rdp(node, timeout, &put_request, &put_response);
+    rpc_cpss_put_block_element(node, timeout, &put_request, &put_response);
 
     if (put_response.result < 0) {
         return -1;
@@ -21,20 +21,20 @@ int32_t cpss_put(int node, uint32_t block_id, void * from, uint32_t length, rpc_
     }
 
 
-    rpc_cpss_put_block_element_rdp_complete_request_t put_complete_request = rpc_cpss_put_block_element_rdp_complete_init(block_id, put_response.vaddr, put_response.size_actual);
-    rpc_cpss_put_block_element_rdp_complete_response_t put_complete_response;
+    rpc_cpss_put_block_element_complete_request_t put_complete_request = rpc_cpss_put_block_element_complete_init(block_id, put_response.vaddr, put_response.size_actual);
+    rpc_cpss_put_block_element_complete_response_t put_complete_response;
 
-    rpc_cpss_put_block_element_rdp_complete(node, timeout, &put_complete_request, &put_complete_response);
+    rpc_cpss_put_block_element_complete(node, timeout, &put_complete_request, &put_complete_response);
 
     return put_response.size_actual;
 }
 
 int32_t cpss_get(void * to, int node, uint32_t block_id, uint32_t length, rpc_protocol_t protocol, int timeout) {
 
-    rpc_cpss_get_block_element_rdp_request_t get_request = rpc_cpss_get_block_element_rdp_init(block_id, length);
-    rpc_cpss_get_block_element_rdp_response_t get_response;
+    rpc_cpss_get_block_element_request_t get_request = rpc_cpss_get_block_element_init(block_id, length);
+    rpc_cpss_get_block_element_response_t get_response;
 
-    int32_t result = rpc_cpss_get_block_element_rdp(node, timeout, &get_request, &get_response);
+    int32_t result = rpc_cpss_get_block_element(node, timeout, &get_request, &get_response);
 
     if (result < 0 || get_response.size_actual == 0) {
         return -1;
@@ -48,10 +48,10 @@ int32_t cpss_get(void * to, int node, uint32_t block_id, uint32_t length, rpc_pr
         return -3;
     }
 
-    rpc_cpss_get_block_element_rdp_complete_request_t get_complete_request = rpc_cpss_get_block_element_rdp_complete_init(block_id, get_response.vaddr, get_response.size_actual);
-    rpc_cpss_get_block_element_rdp_complete_response_t get_complete_response;
+    rpc_cpss_get_block_element_complete_request_t get_complete_request = rpc_cpss_get_block_element_complete_init(block_id, get_response.vaddr, get_response.size_actual);
+    rpc_cpss_get_block_element_complete_response_t get_complete_response;
 
-    rpc_cpss_get_block_element_rdp_complete(node, timeout, &get_complete_request, &get_complete_response);
+    rpc_cpss_get_block_element_complete(node, timeout, &get_complete_request, &get_complete_response);
 
     if (get_complete_response.result < 0) {
         return -2;
